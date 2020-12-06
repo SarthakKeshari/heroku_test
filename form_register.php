3cf2739d04dadce484c7ad1bb360beefef8868f1
SarthakKeshari <srthkkeshari84@gmail.com>
2020-12-06 17:04:25 +0530

    check one

---
+++ form_register.php
@@ -0,0 +1,43 @@
+<?php
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+    $name = $_POST['name'];
+    $pass = $_POST['pass'];
+
+    if (!empty($name) && !empty($pass)) {
+        // $host = "localhost";
+        // $dbusername = "root";
+        // $dbpass = "";
+        // $dbname = "test";
+
+
+        $host = "remotemysql.com";
+        $dbusername = "lFRAvJZ37f";
+        $dbpass = "TiEd2VpnK9";
+        $dbname = "lFRAvJZ37f";
+
+        //create connection
+        $conn = new mysqli($host, $dbusername, $dbpass, $dbname);
+    if (mysqli_connect_error()) {
+    die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
+    } else
+    {
+        $query = "INSERT Into test (name, pass) values('$name','$pass')";
+    $res=mysqli_query($conn,$query);
+    if($res)
+    {
+        $last_id = mysqli_insert_id($conn);
+        echo $last_id;
+    }
+    else
+    {
+        echo "lol";
+        echo mysqli_error($conn);
+    }
+    }
+} else {
+echo "All field are required";
+die();
+}
+}
+?>
---
+++ test.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 06, 2020 at 12:23 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `name` varchar(50) NOT NULL,
+  `pass` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`name`, `pass`) VALUES
+('SARTHAK KESHARI', 'vb');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
